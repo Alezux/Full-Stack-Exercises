47a4407a52292cb148783a119577fec95e7904b3
Alexander <alexander.saukkosaari@gmail.com>
2021-05-27 14:23:45 +0300

    Exercises
    Exercises

---
+++ Part 2/phonebook/src/Persons.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+const baseUrl = './persons'
+
+const getAll = () => (
+    axios
+        .get(baseUrl)
+        .then(response => response.data)
+)
+
+const create = (name, number) => (
+    axios
+        .post(baseUrl, {name, number})
+        .then(response => response.data)
+)
+
+const remove = id => axios.delete(`${baseUrl}/${id}`)
+const update = (id, updatedPerson) => (
+    axios
+        .put(`${baseUrl}/${id}`, updatedPerson)
+        .then(response => response.data)
+)
+
+const exportedObject = {
+    getAll,
+    create,
+    remove,
+    update
+};
+
+export default exportedObject
