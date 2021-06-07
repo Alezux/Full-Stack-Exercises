5e5d6662a1932fba415153c3641dc5ffb4f61dfd
Alexander <alexander.saukkosaari@gmail.com>
2021-06-07 11:47:29 +0300

    Part 3
    Part 3

---
+++ Part 3/phonebook-frontend/src/components/PersonService.js
@@ -0,0 +1,12 @@
+  import axios from 'axios'
+  const baseUrl = 'http://localhost:3001'
+
+const getAllPersons = () => axios.get(baseUrl).then(response => response.data)
+
+const addPerson = (data) => axios.post('http://localhost:3001/api/persons', data).then(response => response.data)
+
+const deletePerson = (id) => axios.delete(`http://localhost:3001/api/persons/${id}`).then(response => response.data)
+
+const updatePerson = (data) => axios.put(`http://localhost:3001/api/persons/${data.id}`, data).then(response => response.data)
+
+export { getAllPersons, addPerson, deletePerson, updatePerson }
